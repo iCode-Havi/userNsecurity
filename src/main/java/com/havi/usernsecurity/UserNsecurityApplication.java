5cfd8f2c1128496588135147eba900eb13d2f60f
siriwardhanasmnp <siriwardhanasmnp.19@uom.lk>
2022-11-05 21:23:19 +0530

    Initial commit

---
+++ src/main/java/com/havi/usernsecurity/UserNsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.havi.usernsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserNsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserNsecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/havi/usernsecurity/UserNsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.havi.usernsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserNsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
